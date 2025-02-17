8ad7384d8bdbec50815fe9567fadb3ba01a0de17
IOLOII <yuey@coinioc.cc>
2025-02-17 11:26:05 +0800

    🌟 update

---
+++ README.md
@@ -0,0 +1,64 @@
+### GitHub Issues Sync Tool Documentation
+
+[English](README.md) | [中文](README.cn.md)
+
+---
+
+#### Overview
+
+This tool is designed to synchronize issues between two GitHub repositories, including the ability to create new issues, update existing ones, and close issues based on their state in the upstream repository. Additionally, it logs all operations for debugging and auditing purposes using a standard logging mechanism that can be viewed with macOS's Console.app.
+
+---
+
+#### Features
+
+1. **Issue Synchronization**: Automatically creates or updates issues in a target repository based on the upstream repository.
+2. **State Handling**: Checks if an issue is closed in the upstream repository and reflects this change in the target repository.
+3. **Logging**: Records all actions performed by the script into a log file and system logs for easy tracking and debugging.
+4. **Environment Configuration**: Uses `.env` files to manage sensitive information such as access tokens securely.
+
+---
+
+#### Setup Instructions
+
+1. **Install Dependencies**
+
+   Ensure you have Node.js installed. Then, install the necessary packages:
+
+   ```bash
+   pnpm i
+   ```
+
+2. **Create .env File**
+
+   In your project root directory, create a `.env` file with the following content:
+
+   ```
+  fork_owner =
+   fork_repo =
+   owner =
+   repo =
+   github_token = ghp_***
+   ```
+
+1. **Run the Script**
+
+   Execute script:
+
+   ```bash
+   npm start
+   ```
+
+---
+
+#### Usage
+
+The script will automatically handle synchronization of issues from the upstream repository to the target repository. It will also ensure that any issues marked as "closed" in the upstream repository are reflected as such in the target repository.
+
+---
+
+#### Logging
+
+All operations performed by the script are logged into `application.log` located in the same directory as the script. Additionally, logs are outputted to the system log, which can be viewed using macOS's Console.app.
+
+---
---
+++ src/main.ts
@@ -0,0 +1,224 @@
+require('dotenv').config()
+
+const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
+const https = require('https')
+
+// 导入 winston 以及 winston-daily-rotate-file
+const { createLogger, format, transports } = require('winston')
+const DailyRotateFile = require('winston-daily-rotate-file')
+
+// 配置授权信息
+const authorization = `Bearer ${process.env.github_token}`
+// 创建一个自定义的https.Agent来忽略SSL证书验证错误
+const agent = new https.Agent({
+  rejectUnauthorized: false, // 注意：这会使连接变得不安全，请谨慎使用。
+})
+
+// 载入或初始化记录文件
+const recordFilePath = path.join(__dirname, 'sync_records.json')
+let syncRecords = {} as {
+  [key: string]: {
+    updated_at: string
+    targetIssueId: number
+    need_update: boolean
+    closed: boolean
+  }
+}
+try {
+  syncRecords = JSON.parse(fs.readFileSync(recordFilePath, 'utf8'))
+} catch (err) {
+  console.log('No existing sync records found or error reading file.')
+}
+
+// 设置日志配置
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.printf((info: any) => `${info.timestamp} [${info.level}]: ${info.message}`)
+  ),
+  transports: [
+    new transports.Console(), // 输出到控制台
+    new transports.File({ filename: path.join(__dirname, 'application.log') }), // 输出到文件
+    new DailyRotateFile({
+      filename: path.join(__dirname, 'logs/application-%DATE%.log'),
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }), // 按日期分割的日志文件
+  ],
+})
+async function fetchIssues(page = 1, since = null) {
+  const url =
+    `https://api.github.com/repos/${process.env.fork_owner}/${process.env.fork_repo}/issues?state=all&per_page=100&page=${page}` +
+    (since ? `&since=${since}` : '')
+  const config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: url,
+    headers: {
+      Accept: 'application/vnd.github+json',
+      'X-GitHub-Api-Version': '2022-11-28',
+      Authorization: authorization,
+    },
+    httpsAgent: agent,
+  }
+
+  try {
+    logger.info(`   Fetching issues for page ${page}`)
+    const response = await axios.request(config)
+    return response.data
+  } catch (error) {
+    logger.info(`   Fetching issues for page ${page}`)
+    console.error(`Error fetching issues for page ${page}:`, error)
+    return []
+  }
+}
+
+async function closeIssue(issueId: string) {
+  const config = {
+    method: 'patch',
+    maxBodyLength: Infinity,
+    url: `https://api.github.com/repos/${process.env.owner}/${process.env.repo}/issues/${issueId}`,
+    headers: {
+      Accept: 'application/vnd.github+json',
+      'X-GitHub-Api-Version': '2022-11-28',
+      'Content-Type': 'application/json',
+      Authorization: authorization,
+    },
+    httpsAgent: agent,
+    data: JSON.stringify({ state: 'closed' }),
+  }
+
+  try {
+    await axios.request(config)
+    console.log(`   Successfully closed issue #${issueId}`)
+  } catch (error) {
+    console.error(`Failed to close issue #${issueId}:`, error)
+  }
+}
+
+async function createOrUpdateIssue(issue: any) {
+  if (issue.pull_request) {
+    console.log(`     Skipping pull request #${issue.number}: ${issue.title}`)
+    return // 过滤掉 pull request
+  }
+
+  const existsInRecords = !!syncRecords[issue.id]
+  const targetIssueId = existsInRecords ? syncRecords[issue.id].targetIssueId : null
+
+  if (!existsInRecords) {
+    // 创建新的 issue 并检查是否需要关闭
+    const data = JSON.stringify({
+      title: issue.title,
+      body: issue.body + '\n' + issue.html_url,
+    })
+
+    const config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: `https://api.github.com/repos/${process.env.owner}/${process.env.repo}/issues`,
+      headers: {
+        Accept: 'application/vnd.github+json',
+        'X-GitHub-Api-Version': '2022-11-28',
+        'Content-Type': 'application/json',
+        Authorization: authorization,
+      },
+      httpsAgent: agent,
+      data: data,
+    }
+
+    try {
+      const response = await axios.request(config)
+      console.log(`Successfully created issue #${response.data.number}: ${response.data.title}`)
+      syncRecords[issue.id] = {
+        updated_at: new Date().toISOString(),
+        targetIssueId: response.data.number,
+        need_update: false,
+        closed: false,
+      } // 更新记录
+
+      // 如果上游issue是关闭状态，则同步关闭下游issue
+      if (issue.state === 'closed' && syncRecords[issue.id].closed !== true) {
+        await closeIssue(response.data.number)
+        syncRecords[issue.id].closed = true
+      }
+    } catch (error) {
+      console.error(`Failed to create issue "${issue.title}":`, error)
+    }
+  } else {
+    // 检查并更新已存在的 issue
+    // 如果上游issue是关闭状态，则同步关闭下游issue
+    if (issue.state === 'closed' && syncRecords[issue.id].closed !== true) {
+      await closeIssue(targetIssueId as unknown as string)
+      syncRecords[issue.id].closed = true
+    } else if (syncRecords[issue.id].need_update) {
+      // 更新其他内容
+      const data = JSON.stringify({
+        title: issue.title,
+        body: issue.body + '\n' + issue.html_url,
+      })
+
+      const config = {
+        method: 'patch',
+        maxBodyLength: Infinity,
+        url: `https://api.github.com/repos/${process.env.owner}/${process.env.repo}/issues/${targetIssueId}`,
+        headers: {
+          Accept: 'application/vnd.github+json',
+          'X-GitHub-Api-Version': '2022-11-28',
+          'Content-Type': 'application/json',
+          Authorization: authorization,
+        },
+        httpsAgent: agent,
+        data: data,
+      }
+
+      try {
+        await axios.request(config)
+        console.log(`Successfully updated issue #${targetIssueId}: ${issue.title}`)
+        syncRecords[issue.id].updated_at = new Date().toISOString()
+        syncRecords[issue.id].need_update = false // 重置更新标记
+        syncRecords[issue.id].closed = false
+      } catch (error) {
+        console.error(`Failed to update issue #${targetIssueId}:`, error)
+      }
+    }
+  }
+}
+
+async function main() {
+  let page = 1
+  let allIssuesFetched = false
+  const since = 'YYYY-MM-DDTHH:MM:SSZ' // 根据需要设置或动态获取
+
+  logger.info('Starting the process of copying and updating issues...')
+  logger.info(`${process.env.fork_owner}/${process.env.fork_repo} ===> ${process.env.owner}/${process.env.repo}`)
+
+  while (!allIssuesFetched) {
+    logger.info(`Starting with page ${page}...`)
+    const issues = await fetchIssues(page)
+
+    if (issues.length === 0) {
+      logger.info('No more issues found. Ending process.')
+      allIssuesFetched = true
+      break
+    }
+
+    for (const issue of issues) {
+      await createOrUpdateIssue(issue)
+    }
+
+    page++
+  }
+
+  // 保存同步记录
+  fs.writeFileSync(recordFilePath, JSON.stringify(syncRecords, null, 2))
+  logger.info('Finished copying and updating issues and updated sync records.')
+}
+
+main().catch(error => {
+  console.error('An unexpected error occurred:', error)
+})
